f201786a4f3ba90f0c17e7c06b34d3baa693adb0
Nsengiyunva <kingbecks07@gmail.com>
2026-01-01 10:46:42 +0300

    corrects the email worker

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,23 +1,21 @@
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
 });
 
-async function sendEmail({ to, subject, text, attachments }) {
-    return transporter.sendMail({
-        from: process.env.SMTP_USER,
-        to,
-        subject,
-        text,
-        attachments
-    });
+export async function sendEmail({ to, subject, text, attachments }) {
+  return transporter.sendMail({
+    from: process.env.SMTP_USER,
+    to,
+    subject,
+    text,
+    attachments
+  });
 }
-
-module.exports = { sendEmail };
