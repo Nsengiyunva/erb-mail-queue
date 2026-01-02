1f524d4a2944da68fef19173026a933de0b98539
Nsengiyunva <kingbecks07@gmail.com>
2026-01-02 16:40:30 +0300

    updates the erb app

--- src/index.js
+++ src/index.js
@@ -1,47 +1,77 @@
 
-import express from "express";
-import * as dotenv from 'dotenv'
-import emailRoutes from './routes/email_routes.js'
-import fileRoutes from './routes/file_routes.js'
+// import express from "express";
+// import * as dotenv from 'dotenv'
+// import emailRoutes from './routes/email_routes.js'
+// import fileRoutes from './routes/file_routes.js'
 
-import monitorRoutes from './routes/monitor_routes.js';
+// import monitorRoutes from './routes/monitor_routes.js';
 
-import './workers/email_workers.js'
-import './workers/file_worker.js'
-import './workers/file_process_worker.js';
-import './workers/file_monitor_worker.js';
+// import './workers/email_workers.js'
+// import './workers/file_worker.js'
+// import './workers/file_process_worker.js';
+// import './workers/file_monitor_worker.js';
 
-import listEndpoints from 'express-list-endpoints';
+// import listEndpoints from 'express-list-endpoints';
 
-import cors from 'cors'
+// import cors from 'cors'
 
-import { connectDB } from "./config/database.js";
+// import { connectDB } from "./config/database.js";
+
+// dotenv.config();
+
+// connectDB();
+
+// const app = express();
+// const PORT = 8782;
+   
+// app.use(express.json());
+
+// app.use( cors() );
+
+// app.use(function (_, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); 
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
+
+
+// console.log("here-1",listEndpoints(app));
+
+// app.use('/api/erb/email', emailRoutes);
+// app.use('/api/erb/file', fileRoutes);
+
+// // app.use('/test/erb/', monitorRoutes);
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+import express from 'express';
+import * as dotenv from 'dotenv';
+import emailRoutes from './routes/email_routes.js';
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
-const PORT = 8782;
-   
+const PORT = process.env.PORT || 8782;
+
 app.use(express.json());
 
-app.use( cors() );
-
-app.use(function (_, res, next) {
+// CORS
+app.use((_, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
-
-console.log("here-1",listEndpoints(app));
-
+// Mount email routes
 app.use('/api/erb/email', emailRoutes);
-app.use('/api/erb/file', fileRoutes);
 
-// app.use('/test/erb/', monitorRoutes);
+// Optional test route
+app.get('/api/test', (req, res) => res.json({ ok: true }));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
