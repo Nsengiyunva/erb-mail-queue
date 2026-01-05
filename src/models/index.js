2ad103ace6294c8e5b56869deef801544c0c2a47
Nsengiyunva <kingbecks07@gmail.com>
2026-01-05 15:34:50 +0300

    updates the email workers

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,18 @@
 // models/index.js
+import Sequelize from 'sequelize';
 import FileBatch from './FileBatch.js';
 import ProcessedFile from './ProcessedFile.js';
-import EmailLog from './EmailLog.js';
+import EmailLogModel from './EmailLog.js'; // note renaming
 import { sequelize } from '../config/database.js';
 
+// Initialize EmailLog properly
+const EmailLog = EmailLogModel(sequelize, Sequelize.DataTypes);
 
+// Associations
 FileBatch.hasMany(ProcessedFile, { foreignKey: 'batchId' });
 ProcessedFile.belongsTo(FileBatch, { foreignKey: 'batchId' });
 
+// Export initialized models
 export {
   sequelize,
   FileBatch,
