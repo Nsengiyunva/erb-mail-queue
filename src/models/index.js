6bf8b8247138084f3bb5ae8301daf5bd4d9734b7
Nsengiyunva <kingbecks07@gmail.com>
2026-03-27 12:43:48 +0300

    updates the model index

--- src/models/index.js
+++ src/models/index.js
@@ -3,11 +3,12 @@ import Sequelize from 'sequelize';
 import FileBatch from './FileBatch.js';
 import ProcessedFile from './ProcessedFile.js';
 import EmailLogModel from './EmailLog.js';
-import Application from './Application.js'
+import ApplicationModel from './Application.js'; // ← renamed to clarify it's a factory
 import { sequelize } from '../config/database.js';
 
-// Initialize EmailLog properly
+// Initialize models that use factory pattern
 const EmailLog = EmailLogModel(sequelize, Sequelize.DataTypes);
+const Application = ApplicationModel(sequelize, Sequelize.DataTypes); // ← add this
 
 // Associations
 FileBatch.hasMany(ProcessedFile, { foreignKey: 'batchId' });
